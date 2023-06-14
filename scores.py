7c52ead98dbbd3916156b000dc54727c7a13dd24
Piotr Barabasz <barabasz.piotrek@gmail.com>
2023-06-14 18:54:18 +0200

    fix tomek_smote_knn3_ppl

--- scores.py
+++ scores.py
@@ -20,61 +20,35 @@ table = tabulate(np.mean(scores, axis=-1),
                  ])
 
 
-print(tabulate(scores))
 # index 1. datasets
 # index 2. classificators
-print(scores[0, 0, :])
-print(scores[1, 0, :])
-print(scores.shape)
+# print(scores[0, 0, :])
+# print(scores[1, 0, :])
+# print(scores.shape)
 result = []
 for dataset_idx in range(scores.shape[0]):
-    # dataset_idx = 0
-    # print('dataset: ', dataset_idx)
+    dataset_result = []
     for classifier_idx in range(scores.shape[1]):
+        classifier_result = []
         for compare_classifier_idx in range(scores.shape[1]):
-            # print(classifier_idx, ' : ', compare_classifier_idx)
-            result.append(ttest_rel(
+            classifier_result.append(ttest_rel(
                 scores[dataset_idx, classifier_idx, :],
                 scores[dataset_idx, compare_classifier_idx, :]
             ))
+        dataset_result.append(classifier_result)
+    result.append(dataset_result)
 
-for i in range(len(result)):
-    print(result[i].statistic)
-# print(result.statistic)
-# print(result.pvalue)
-# print(tabulate(result))
+
+result_array = np.array(result)
+
+# result_statistic = (result_array[:, :, :, 0] < 0.05).astype(int)
+result_pvalue = (result_array[:, :, :, 1] < 0.05).astype(int)
+
+# 0 statistic
+# 1 pvalue
+result_statistic = (result_array[:, :, :, 0])
+# result_pvalue = (result_array[:, :, :, 1])
+
+print(tabulate(result_statistic))
+print(tabulate(result_pvalue))
 exit()
-
-result = np.zeros((6, 10))
-for x_index in range(6):
-    for y_index in range(9):
-        ttest_result = ttest_rel(scores[x_index, y_index, :], scores[x_index, y_index + 1, :])
-        result[x_index, y_index] = ttest_result.pvalue
-
-print(tabulate(result))
-print(result.shape)  # Output: (6, 10)
-
-
-exit()
-print(table)
-res_statistic = []
-res_pvalue = []
-for dataset_idx in range(np.shape(scores)[0]):
-    for classifier_idx in range(np.shape(scores)[1] - 1):
-        for next_classifier_idx in range(classifier_idx + 1, np.shape(scores)[1]):
-            # print(dataset_idx, classifier_idx, ' compare to ', dataset_idx, next_classifier_idx)
-            result = ttest_rel(
-                scores[dataset_idx, classifier_idx, :],
-                scores[dataset_idx, next_classifier_idx, :]
-            )
-            res_statistic.append(result.statistic < 0.05)
-            res_pvalue.append(result.pvalue < 0.05)
-            # ktores z tych
-
-            # print(result)
-            # print('result.statistic', result.statistic)
-            # print('result.pvalue ', result.pvalue)
-# print('result.statistic', result.statistic)
-# print('result.pvalue ', result.pvalue)
-print(res_pvalue)
-print(res_statistic)
